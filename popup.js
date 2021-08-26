19b5b4a0d7867cd9c5ba980858377cbbec0f9b79
Q <roussetquent1@gmail.com>
2021-08-25 23:29:33 -0400

    something works

--- popup.js
+++ popup.js
@@ -1,11 +1,18 @@
 // When the button is clicked, inject setPageBackgroundColor into current page
-const pullData = document.getElementById('fetch Events')
-pullData.addEventListener("click", async () => {
+const pullData = document.getElementById("fetchEvents");
+pullData.addEventListener("click", () => {
+  const textbox = document.querySelector(".container");
 
-  const textbox = document.querySelector('.main')
-  fetch('https://en.wikipedia.org/api/rest_v1/feed/onthisday/events/08/25')
-  .then((data) => data.json())
-  .then((data) => {
-    console.log(data)
-  })
+  fetch("https://en.wikipedia.org/api/rest_v1/feed/onthisday/events/08/26")
+    .then((data) => data.json())
+    .then((data) => {
+      data['events'].forEach(element => createMessage(element, textbox));
+    });
 });
+
+function createMessage(message, body) {
+  const newEvent = document.createElement("p");
+  newEvent.className = "event";
+  newEvent.innerText = message.text;
+  body.appendChild(newEvent);
+}
